662cd8ac521129d39ff05766c915813f670d474c
Claholla <claholla@gmail.com>
2021-05-27 20:25:41 -0500

    Added thecocktaildb API call function and second JS file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,5 @@
 // Document variable declarations
 let userInput = document.getElementById("searchinput");
-
 let searchBtn = document.getElementById("searchbtn");
 let edamamId = "15c45754";
 let edamamKey = "27854137d14a75271ecad8471fbc5826";
@@ -8,6 +7,10 @@ let titleStorage = [];
 let photoStorage = [];
 let linkStorage = [];
 let sourceStorage = [];
+let drinkNameStorage = [];
+let drinkPhotoStorage = [];
+let drinkGlassStorage = [];
+let drinkURLStorage = [];
 
 // Search button event listener
 searchBtn.addEventListener("click", function() {
@@ -21,14 +24,19 @@ searchBtn.addEventListener("click", function() {
     photoStorage = [];
     linkStorage = [];
     sourceStorage = [];
-
+    drinkNameStorage = [];
+    drinkPhotoStorage = [];
+    drinkGlassStorage = [];
+    drinkURLStorage = [];
+    // foodCall(userInput);
+    drinkCall(userInput);
+})
 
 // Edamam food search API call
 function foodCall() {
     fetch("http://api.edamam.com/search?app_id=" + edamamId + "&app_key=" + edamamKey + "&q=" + userInput.value)
     .then (response => response.json())
     .then(data => {
-
         document.location.href="./results.html";
         console.log(data);
         // Loop to store data for population of results page
@@ -47,14 +55,37 @@ function foodCall() {
         localStorage.setItem("Photos", photoStorage);
         localStorage.setItem("Links", linkStorage);
         localStorage.setItem("Sources", sourceStorage);
-
         }
     )
 }
 
-// Suggested search functions
+// TheCocktailDB drink search API call
+function drinkCall() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + userInput.value)
+    .then (response => response.json())
+    .then (data => {
+        //document.location.href="./results.html";
+        console.log(data);
+        // Loop to store data for population of results page
+        for (let x=0; x < data["drinks"].length; x++) {
+            let drinkName = data["drinks"][x]["strDrink"];
+            let drinkPhoto = data["drinks"][x]["strDrinkThumb"];
+            let drinkGlass = data["drinks"][x]["strGlass"];
+            let drinkURL = "https://www.thecocktaildb.com/drink/" + data["drinks"][x]["idDrink"];
+            drinkNameStorage.push(drinkName);
+            drinkPhotoStorage.push(drinkPhoto);
+            drinkGlassStorage.push(drinkGlass);
+            drinkURLStorage.push(drinkURL);
+        }
+        // Storage of returned API call data
+        localStorage.setItem("drinkNames", drinkNameStorage);
+        localStorage.setItem("drinkPhotos", drinkPhotoStorage);
+        localStorage.setItem("drinkGlasses", drinkGlassStorage);
+        localStorage.setItem("drinkURLs", drinkURLStorage);
+    })
+}
 
+// Suggested search functions
 function randomFood() {
 
 }
-
