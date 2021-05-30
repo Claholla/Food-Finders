f53b8fb5851a900029d19e2c559e5db1b2b159db
Claholla <claholla@gmail.com>
2021-05-29 23:14:10 -0500

    Finished functionality of all search functions

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -1,27 +1,51 @@
-// DOM selector variables
+// Document DOM selector variables
+let container = document.getElementById("container");
+let cardclone = document.getElementById("cardclone");
 
+// Arrays to contain the localStorage data used for populating the page
+let drinkNames = JSON.parse(localStorage.getItem("drinkNames"));
+let drinkGlasses = JSON.parse(localStorage.getItem("drinkGlasses"));
+let drinkPhotos = JSON.parse(localStorage.getItem("drinkPhotos"));
+let drinkURLs = JSON.parse(localStorage.getItem("drinkURLs"));
+let titles = JSON.parse(localStorage.getItem("Titles"));
+let photos = JSON.parse(localStorage.getItem("Photos"));
+let links = JSON.parse(localStorage.getItem("Links"));
+let sources = JSON.parse(localStorage.getItem("Sources"));
 
 // Function to populate search results using localStorage data
 window.onload = function() {
     // Statement determines which API was called through defined localStorage objects
-    if (localStorage.drinkNames) {
-        // Arrays to contain the localStorage objects for populating page
-        let drinkNames = JSON.parse(localStorage.getItem("drinkNames"));
-        let drinkGlasses = JSON.parse(localStorage.getItem("drinkGlasses"));
-        let drinkPhotos = JSON.parse(localStorage.getItem("drinkPhotos"));
-        let drinkURLs = JSON.parse(localStorage.getItem("drinkURLs"));
-        console.log(drinkNames);
+    if (localStorage.getItem("drinkNames")) {
         // Loop clones the search result card and populates the clones
         for (y=0; y < drinkNames.length; y++) {
-            let container = document.getElementById("container");
-            let cardclone = document.getElementById("cardclone");
             document.getElementById("photo").setAttribute("src", drinkPhotos[y]);
             document.getElementById("maintext").innerHTML = drinkNames[y];
             document.getElementById("subtext").innerHTML = drinkGlasses[y];
             document.getElementById("link").setAttribute("href", drinkURLs[y]);
             let cln = cardclone.cloneNode(true);
             container.append(cln);
-            
         }
-        document.getElementById("cardclone").setAttribute("display", "none");
-    }}
+        // Deletes source for cloned results
+        document.getElementById("cardclone").remove();
+
+    } else if (localStorage.getItem("Titles")) {
+        // Loop clones the search result card and populates the clones
+        for (x=0; x < titles.length; x++) {
+            document.getElementById("photo").setAttribute("src", photos[x]);
+            document.getElementById("maintext").innerHTML = titles[x];
+            document.getElementById("subtext").innerHTML = "Source: " + sources[x];
+            document.getElementById("link").setAttribute("href", links[x]);
+            let cln = cardclone.cloneNode(true);
+            container.append(cln);
+        }
+        // Deletes source for cloned results
+        document.getElementById("cardclone").remove();
+
+    } else {
+        document.getElementById("cardclone").remove();
+        let container = document.getElementById("container");
+        let error = document.createElement("h1");
+        error.setAttribute("class", "column");
+        error.innerHTML = "No results found.";
+        container.append(error);
+}}
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,10 @@
 // Document variable declarations
 let userInput = document.getElementById("searchinput");
 let searchBtn = document.getElementById("searchbtn");
+let food = document.getElementById("food");
+let drinks = document.getElementById("drinks");
+let ranFood = document.getElementById("foodbtn");
+let ranDrink = document.getElementById("drinkbtn");
 let edamamId = "15c45754";
 let edamamKey = "27854137d14a75271ecad8471fbc5826";
 let titleStorage = [];
@@ -12,12 +16,24 @@ let drinkPhotoStorage = [];
 let drinkGlassStorage = [];
 let drinkURLStorage = [];
 
+// Search type selection button event listeners
+food.addEventListener("click", function() {
+    localStorage.setItem("searchSelect", "Food");
+    userInput.setAttribute("placeholder", "Search for Food!");
+})
+
+drinks.addEventListener("click", function () {
+    localStorage.setItem("searchSelect", "Drinks");
+    userInput.setAttribute("placeholder", "Search for Drinks!");
+})
+
 // Search button event listener
 searchBtn.addEventListener("click", function() {
     // Prevents the user from activating the function without input for query
     if (userInput.value < 1) {
         return;
-    } else {
+
+    } else if (localStorage.getItem("searchSelect") == "Food") {
         // Clears data from storage for multiple searches
         localStorage.clear();
         // Clears arrays for use in populating localStorage multiple times
@@ -29,9 +45,47 @@ searchBtn.addEventListener("click", function() {
         drinkPhotoStorage = [];
         drinkGlassStorage = [];
         drinkURLStorage = [];
-        // foodCall(userInput);
+        foodCall(userInput);
+        
+    } else if (localStorage.getItem("searchSelect") == "Drinks") {
+        localStorage.clear();
+        titleStorage = [];
+        photoStorage = [];
+        linkStorage = [];
+        sourceStorage = [];
+        drinkNameStorage = [];
+        drinkPhotoStorage = [];
+        drinkGlassStorage = [];
+        drinkURLStorage = [];
         drinkCall(userInput);
     }
+})
+
+// Random API call button event listeners
+ranDrink.addEventListener("click", function () {
+    localStorage.clear();
+    titleStorage = [];
+    photoStorage = [];
+    linkStorage = [];
+    sourceStorage = [];
+    drinkNameStorage = [];
+    drinkPhotoStorage = [];
+    drinkGlassStorage = [];
+    drinkURLStorage = [];
+    randomDrink();
+})
+
+ranFood.addEventListener("click", function() {
+    localStorage.clear();
+    titleStorage = [];
+    photoStorage = [];
+    linkStorage = [];
+    sourceStorage = [];
+    drinkNameStorage = [];
+    drinkPhotoStorage = [];
+    drinkGlassStorage = [];
+    drinkURLStorage = [];
+    randomFood();
 })
 
 // Edamam food search API call
@@ -39,25 +93,29 @@ function foodCall() {
     fetch("http://api.edamam.com/search?app_id=" + edamamId + "&app_key=" + edamamKey + "&q=" + userInput.value)
     .then (response => response.json())
     .then(data => {
-        document.location.href="./results.html";
-        console.log(data);
-        // Loop to store data for population of results page
-        for (let i=0; i < 10; i++) {
-            let recipeTitle = data["hits"][i]["recipe"]["label"];
-            let recipePhoto = data["hits"][i]["recipe"]["image"];
-            let recipeLink = data["hits"][i]["recipe"]["url"];
-            let recipeSource = data["hits"][i]["recipe"]["source"];
-            titleStorage.push(recipeTitle);
-            photoStorage.push(recipePhoto);
-            linkStorage.push(recipeLink);
-            sourceStorage.push(recipeSource);
-        }
-        // Storage of returned API call data
-        localStorage.setItem("Titles", JSON.stringify(titleStorage));
-        localStorage.setItem("Photos", JSON.stringify(photoStorage));
-        localStorage.setItem("Links", JSON.stringify(linkStorage));
-        localStorage.setItem("Sources", JSON.stringify(sourceStorage));
-        }
+        // When API returns no relevant data this displays the results not found page
+        if (data["count"] == 0) {
+            localStorage.clear();
+            document.location.href="./results.html";    
+        } else {
+            document.location.href="./results.html";
+            // Loop to store data for population of results page
+            for (let i=0; i < data["hits"].length; i++) {
+                let recipeTitle = data["hits"][i]["recipe"]["label"];
+                let recipePhoto = data["hits"][i]["recipe"]["image"];
+                let recipeLink = data["hits"][i]["recipe"]["url"];
+                let recipeSource = data["hits"][i]["recipe"]["source"];
+                titleStorage.push(recipeTitle);
+                photoStorage.push(recipePhoto);
+                linkStorage.push(recipeLink);
+                sourceStorage.push(recipeSource);
+            }
+            // Storage of returned API call data
+            localStorage.setItem("Titles", JSON.stringify(titleStorage));
+            localStorage.setItem("Photos", JSON.stringify(photoStorage));
+            localStorage.setItem("Links", JSON.stringify(linkStorage));
+            localStorage.setItem("Sources", JSON.stringify(sourceStorage));
+            }}
     )
 }
 
@@ -67,7 +125,6 @@ function drinkCall() {
     .then (response => response.json())
     .then (data => {
         document.location.href="./results.html";
-        console.log(data);
         // Loop to store data for population of results page
         for (let x=0; x < data["drinks"].length; x++) {
             let drinkName = data["drinks"][x]["strDrink"];
@@ -87,7 +144,52 @@ function drinkCall() {
     })
 }
 
-// Suggested search functions
-function randomFood() {
+// Randomized results button functions
+function randomDrink() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then (response => response.json())
+    .then (data => {
+        document.location.href="./results.html";
+        // Stores the data for population of results page
+        let drinkName = data["drinks"][0]["strDrink"];
+        let drinkPhoto = data["drinks"][0]["strDrinkThumb"];
+        let drinkGlass = data["drinks"][0]["strGlass"];
+        let drinkURL = "https://www.thecocktaildb.com/drink/" + data["drinks"][0]["idDrink"];
+        drinkNameStorage.push(drinkName);
+        drinkPhotoStorage.push(drinkPhoto);
+        drinkGlassStorage.push(drinkGlass);
+        drinkURLStorage.push(drinkURL);
+        // Storage of returned API call data
+        localStorage.setItem("drinkNames", JSON.stringify(drinkNameStorage));
+        localStorage.setItem("drinkPhotos", JSON.stringify(drinkPhotoStorage));
+        localStorage.setItem("drinkGlasses", JSON.stringify(drinkGlassStorage));
+        localStorage.setItem("drinkURLs", JSON.stringify(drinkURLStorage));
+    })
+}
 
+function randomFood() {
+    // A bank of search queries to be randomized for the suggested random result button
+    let searchBank = ["Tacos", "Taco", "Pizza", "Pasta", "Meatball", "Quiche", "Brownie", "Cake", "Quesadilla", "Pie", "Bread", "Soup", "Stew", "Pudding"]
+    let randomizer = Math.floor(Math.random() * searchBank.length);
+    // Edamam API call using the result of the random bank result
+    fetch("http://api.edamam.com/search?app_id=" + edamamId + "&app_key=" + edamamKey + "&q=" + searchBank[randomizer])
+    .then (response => response.json())
+    .then (data => {
+        document.location.href="./results.html";
+        // Randomizes a selection from the random API call's data array
+        let randomRecipe = Math.floor(Math.random() * data["hits"].length);
+        let recipeTitle = data["hits"][randomRecipe]["recipe"]["label"];
+        let recipePhoto = data["hits"][randomRecipe]["recipe"]["image"];
+        let recipeLink = data["hits"][randomRecipe]["recipe"]["url"];
+        let recipeSource = data["hits"][randomRecipe]["recipe"]["source"];
+        titleStorage.push(recipeTitle);
+        photoStorage.push(recipePhoto);
+        linkStorage.push(recipeLink);
+        sourceStorage.push(recipeSource);
+        // Storage of returned API call data
+        localStorage.setItem("Titles", JSON.stringify(titleStorage));
+        localStorage.setItem("Photos", JSON.stringify(photoStorage));
+        localStorage.setItem("Links", JSON.stringify(linkStorage));
+        localStorage.setItem("Sources", JSON.stringify(sourceStorage));
+    })
 }
